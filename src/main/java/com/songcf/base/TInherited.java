8eb6c9120d57f0c84a2e4fdb599975dc3507c0ad
songcf <cheungfook.song@gmail.com>
2019-02-08 16:21:28 +0800

    add abs test

--- src/main/java/com/songcf/base/TInherited.java
+++ src/main/java/com/songcf/base/TInherited.java
@@ -5,8 +5,18 @@ import com.songcf.exception.*;
 
 public class TInherited {
     public static void test() {
+
+        //
         Base b1 = new BaseChild1();
         Base b2 = new BaseChild1("b2");
+        (new Base()).test();
+        b1.test();
+
+        //
+        Abs a1 = new AbsChild1();
+        a1.test();
+        a1.test(1);
+        a1.test("I am Cheff");
     }
 }
 
@@ -36,12 +46,12 @@ class Base {
     }
 }
 
-class BaseChild1 extends  Base{
+class BaseChild1 extends Base {
     BaseChild1() {
         System.out.println("BaseChild1 constructor1");
     }
     BaseChild1(String arg) {
-        super(arg);
+        super(arg); //如果没有调用，将会调用默认的父类构造器
         System.out.println("BaseChild1 constructor2 arg");
     }
 
@@ -55,8 +65,23 @@ class BaseChild1 extends  Base{
     }
 }
 
-abstract class Abs1 {
+abstract class Abs {
+    abstract public void test();
+    public void test(Integer i) {
+        System.out.println("Abs:test Integer arg " + i);
+    }
+    public void test(String str) {
+        System.out.println("Abs:test String arg " + str);
+    }
+}
 
+class AbsChild1 extends Abs{
+    public void test() {
+        System.out.println("AbsChild1:test");
+    }
+    public void test(Integer i) {
+        System.out.println("AbsChild1:test Integer arg " + i);
+    }
 }
 
 interface Interface1 {
